9f36416b67b634a50ad8fb684fbfe4b7ba82bcb0
Hethini <hethini.ribeiro@gmail.com>
2022-08-14 12:17:07 -0300

    Adding livros CRUD

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const http = require("http")
-const port = 3000;
+import app from "./src/app.js";
+const port = process.env.PORT || 3000;
 
-const rota = {
-    '/': 'Estudando node',
-    '/livros': 'Pagina de livros',
-    '/autores': 'Lista de autores',
-    '/editora': 'Pagina de editora',
-    '/sobre': 'Infos do projeto'
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rota[req.url]);
-})
-
-server.listen(port, () => {
-    console.log('Servidor escutando em http://localhost:',port);
+app.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`,);
 });
